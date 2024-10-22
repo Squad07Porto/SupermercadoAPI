9d05fc51f01fb1c0817c97bcf6492256935da995
Pedro <santiagvs@proton.me>
2024-10-22 19:39:53 -0300

    fix: renomeia a coluna 'descricao' para 'nome' e adiciona ao model a coluna 'quantidade'

--- Supermercado.API/Models/DTO/ProdutoDTO.cs
+++ Supermercado.API/Models/DTO/ProdutoDTO.cs
@@ -2,11 +2,10 @@ namespace Supermercado.API.Models.DTO
 {
   public class ProdutoDTO
   {
-    public int Id { get; set; }
     public string? Codigo { get; set; }
-    public string? Descricao { get; set; }
+    public string? Nome { get; set; }
     public decimal Preco { get; set; }
-    public int SecaoId { get; set; }
-    public string? SecaoDescricao { get; set; }
+    public int Quantidade { get; set; }
+    public int? SecaoId { get; set; }
   }
 }
--- Supermercado.API/Models/Produto.cs
+++ Supermercado.API/Models/Produto.cs
@@ -4,8 +4,9 @@ namespace Supermercado.API.Models
     {
         public int Id { get; set; }
         public string Codigo { get; set; } = string.Empty;
-        public string Descricao { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
         public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
         public int SecaoId { get; set; }
         public virtual Secao Secao { get; set; } = new Secao();
     }
