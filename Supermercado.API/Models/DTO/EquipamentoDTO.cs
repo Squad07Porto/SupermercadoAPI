ab2ebf2bfa9cc778b2badaed4949821f2b655cb9
Pedro <santiagvs@proton.me>
2024-10-22 19:54:32 -0300

    fix: retirando Id do DTO e campos estáticos da tabela de Secao e adicionando o campo de SecaoId para referenciar

--- Supermercado.API/Models/DTO/EquipamentoDTO.cs
+++ Supermercado.API/Models/DTO/EquipamentoDTO.cs
@@ -2,10 +2,8 @@ namespace Supermercado.API.Models.DTO
 {
     public class EquipamentoDTO
     {
-        public int Id { get; set; }
         public string? TipoEquipamento { get; set; }
         public string? Descricao { get; set; }
-        public int SecaoId { get; set; }
-        public string? SecaoDescricao { get; set; }
+        public int? SecaoId { get; set; }
     }
 }
